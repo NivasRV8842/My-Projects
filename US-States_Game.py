2cd666e06a44a99b09f17903f7c251ba29c801fe
Nivas R V <90444294+NivasRV8842@users.noreply.github.com>
2024-09-21 23:38:15 +0530

    Add files via upload

---
+++ US-States_Game.py
@@ -0,0 +1,13 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_state_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+with open("us-states-game-start/50_states.csv")
+
+screen.exitonclick()
